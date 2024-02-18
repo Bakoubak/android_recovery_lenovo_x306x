590a357fa293edc7ec405fd7415e80ae762dcdf3
VistaSlayer <Bakoubak85@gmail.com>
2024-02-18 11:49:03 +0100

    device: Add device.mk

---
+++ device.mk
@@ -0,0 +1,15 @@
+# Screen density
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 800
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+# Dynamic partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
